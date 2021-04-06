49b048520f5e61a3f002d50c374c055e1d492698
VijayUndrajavarapu <vijayundrajavarapu@gmail.com>
2021-04-06 09:08:22 +0530

    TejoRajesh_lap

---
+++ TejoRajesh/src/ChantsLap/Test2.java
@@ -0,0 +1,10 @@
+package ChantsLap;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("chanti's laptop eclise test");
+	}
+
+}
