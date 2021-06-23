7a2ff38c443de86c22ce36473ceddd7fe57225d8
VijayUndrajavarapu <54445371+VijayUndrajavarapu@users.noreply.github.com>
2021-06-23 11:29:27 -0700

    ng

--- May_03/src/Started_prac/TestNG_TC_002.java
+++ May_03/src/Started_prac/TestNG_TC_002.java
@@ -11,9 +11,17 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class TestNG_TC_002 {
-  @Test
-  public void f() {
-	  System.out.println("f");
+  @Test(priority=3)
+  public void test1() {
+	  System.out.println("test1");
+  }
+  @Test(priority=2,enabled= false)
+  public void test2() {
+	  System.out.println("test2");
+  }
+  @Test(priority=1)
+  public void test3() {
+	  System.out.println("test3");
   }
   @BeforeMethod
   public void beforeMethod() {	
